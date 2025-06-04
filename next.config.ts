314a421e98bcd6c1620a5d7f94780d2564338473
Edesius Jaty Giovanni <edesiusjatyg@gmail.com>
2025-06-04 13:01:36 +0700

    added prisma client prevention from being bundled to client-side on next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.externals = {
+      ...config.externals,
+      '@prisma/client': 'commonjs @prisma/client',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
